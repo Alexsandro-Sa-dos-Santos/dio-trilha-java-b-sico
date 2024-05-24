903055861fb134c8a0eb2bdad6a3c8a41f7336a1
Alexsandro-Sa-dos-Santos <alexsandro.gf@hotmail.com>
2024-05-24 00:55:30 -0300

    projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agenciaCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo: ");
+        double saldo = scanner.nextDouble();
+        //Obter pela scanner os valores digitados no terminal
+        
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaCliente + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
